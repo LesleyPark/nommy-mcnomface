76fa2ee79f59595d5c63e65e1cc065ccd49bc576
Lesley Park <lesparkley@gmail.com>
2016-10-09 15:45:05 +0000

    Reset database structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009042212) do
+ActiveRecord::Schema.define(version: 20161009054906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
